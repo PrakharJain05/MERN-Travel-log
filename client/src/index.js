a8bf878711c38abfdf0dffc104b33e51d363ceee
Prakhar Jain <prakharj05@gmail.com>
2020-08-19 16:45:01 +0530

    uploaded on web

--- client/src/index.js
+++ client/src/index.js
@@ -1,41 +1,11 @@
-const express = require('express');
-const morgan = require('morgan');
-const helmet = require('helmet');
-const cors = require('cors');
-const mongoose = require('mongoose');
-require('dotenv').config()
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-const middleware = require('./middleware');
-const logs = require('./routes/logs')
-
-const app = express()
-
-mongoose.connect(process.env.DATABASE_URL,{
-    useNewUrlParser:true,
-    useUnifiedTopology: true 
-})
-
-app.use(morgan('common'))
-app.use(helmet())
-app.use(cors({
-    origin:process.env.CORS_ORIGIN
-}))
-
-app.use(express.json())
-
-app.get('/', (req,res) => {
-    res.json({
-        message : "Hello"
-    })
-})
-
-app.use('/routes/logs',logs)
-
-app.use(middleware.notFound)
-app.use(middleware.errorHandler)
-
-const port = process.env.PORT || 1337;
-
-app.listen(port, () => {
-    console.log('Connected')
-})
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
